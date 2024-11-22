8b6244aff5ab0d5cbaec6640b8b266c893962e6f
vigneshwaran <vigneshwaran.s@newstreettech.com>
2024-11-22 11:45:11 +0530

    Animation implemented.

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -1,5 +1,7 @@
 package com.plcoding.bookpedia.app
 
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -36,7 +38,10 @@ fun App() {
                 startDestination = Route.BookList
             ) {
 
-                composable<Route.BookList> {
+                composable<Route.BookList>(
+                    exitTransition = { slideOutHorizontally() },
+                    popEnterTransition = { slideInHorizontally() }
+                ) {
                     val viewModel = koinViewModel<BookListViewModel>()
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController)
@@ -53,7 +58,20 @@ fun App() {
                     )
                 }
 
-                composable<Route.BookDetail> {
+                composable<Route.BookDetail>(
+                    enterTransition = {
+                        slideInHorizontally { initialOffset ->
+                            initialOffset
+
+                        }
+                    },
+                    exitTransition = {
+                        slideOutHorizontally { initialOffset ->
+                            initialOffset
+
+                        }
+                    }
+                ) {
                     val viewModel = koinViewModel<BookDetailViewModel>()
                     val selectedBookViewModel =
                         it.sharedKoinViewModel<SelectedBookViewModel>(navController)
